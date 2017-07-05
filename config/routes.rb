ca46e7b39082b8bcc56e8464756f9cce3c3aa11a
xiaowu <xiaohuiwu521@gmail.com>
2017-07-05 23:17:14 +0800

    implement conversation histroy

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   resources :users,only:[:index]
 
-  resources :conversations,only:[:create] do
+  resources :conversations,only:[:index,:create] do
      resources :messages
    end
 
